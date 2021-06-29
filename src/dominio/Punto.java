913df50bb3b8603c08e6ce41880dff74e05eb900
Erick Mera <eamera10@outlook.com>
2021-06-29 14:32:20 -0500

    Correccion Examen

---
+++ src/dominio/Punto.java
@@ -0,0 +1,51 @@
+
+package dominio;
+
+public class Punto {
+    private int x;
+    int y;
+    int distancia;
+    
+    public Punto (int x,int y){
+    this.x=x;
+    this.y=y;
+    }
+    
+    public void moverArriba(int aumento){
+        y=y+aumento;
+    }
+    
+    public void moverAbajo(int aumento){
+        y=y-aumento;
+    }
+    
+    public void moverDerecha(int aumento){
+    x+=aumento;
+    }
+    
+    public void moverIzquierda(int aumento){
+    x-=aumento;
+    }
+    
+    public int getX(){
+        return x;
+    }
+    
+    public int getY(){
+        return y;
+    }
+    
+    public double calculoDistancia( Punto P1, Punto P2){
+        int x1=P2.getX();
+        int y1=P2.getY();
+        int x2=P1.getX();
+        int y2=P1.getY();
+        int r1,r2;
+        r1=x2-x1;
+        r2=y2-y1;
+        double z1 = Math.pow(r1,2);
+        double z2 = Math.pow(r1,2);
+        double distancia = Math.sqrt(z1+z2);
+        return distancia;
+    }
+}
---
+++ src/test/PruebaPunto.java
@@ -0,0 +1,46 @@
+
+package test;
+
+import dominio.Punto;
+import java.util.Scanner;
+
+public class PruebaPunto {
+      public static void main(String[] args) {
+        Punto P1;
+        Punto P2;
+        Scanner entrada=new Scanner(System.in);
+        int x=0;
+        int y=0;
+        int opcion=0;
+        int aumento=0;
+        int cantidadM=0,contadorM=0;
+        P1=new Punto(x,y);
+        P2=new Punto(x,y);
+        System.out.println("\n La Posicion Actual (X;Y) es : ("+P1.getX()+", "+P1.getY()+")");//Posicion inicial (0,0)
+        
+        System.out.println("\n\t. Menú.\n");
+        System.out.print("Por Favor Ingrese la cantidad de Movimientos que desea realizar : ");//Ingreso de la cantidad de movimientos
+        cantidadM=entrada.nextInt();
+        System.out.print("Por favor Ingrese la cantidad de espacios a moverse: ");//Ingreso de los movimientos
+        aumento=entrada.nextInt();
+       
+                do {
+                //Genero movimiento arriba, abajo, izquierda,derecha aleatorios entre 1 y 4
+                double movimiento=Math.random()*3+1;
+                opcion=(int)movimiento;
+                switch(opcion){
+                    case 1: P1.moverArriba(aumento); break;
+                    case 2: P1.moverAbajo(aumento); break;
+                    case 3: P1.moverDerecha(aumento); break;
+                    case 4: P1.moverIzquierda(aumento); break;
+                    case 5: break;
+                    default: System.out.println("Error"); break;
+                }     
+
+                System.out.println("\n Posicion (X;Y): ("+P1.getX()+", "+P1.getY()+")"); //Posicion despues de realizar los movimientos
+                contadorM++; 
+                    } while (contadorM<cantidadM); 
+    }
+    
+} 
+
